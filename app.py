8c6661b6785bbef93cfa0b6b9ba45e2d6a609360
Helen Prasad <76021410+helenp61@users.noreply.github.com>
2024-04-21 12:08:39 +0000

    New file on tkinter

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
 
-from tkinter import ttk  #for widgets and buttons
+#for widgets and buttons
 
 root = tk.Tk()   #the main window
+ttk.Label(root, text="Hello World!", padding=(30, 10)).pack()
+#label will be inside the main root(parent) and then telling it to pack itself into the parent
+#pack is a geometry manager that tells the widget to be packed into the parent
+
 
 root.mainloop()
 
