0ca8acc7f0807de903e8e05223573ccae83a70b2
Helen Prasad <76021410+helenp61@users.noreply.github.com>
2024-04-22 00:07:21 +0000

    Changed names to reflect order in which work done.

--- 03_buttons.py
+++ 03_buttons.py
@@ -11,6 +11,9 @@ root.title("Hello")
 
 greet_button = ttk.Button(root, text="Greet", command=greet)
 #pass in a text and commands
+#command is a function that will be called when the button is pressed
+#fill makes the button expand to fill the entire width of the parent
+#expand makes the button expand to fill the entire height of the parent
 greet_button.pack(side="left", fill="x", expand=True)
 
 quit_button = ttk.Button(root, text="Quit", command=root.destroy)
